ef5538efbce0d3e82d01e72c7ed0896f35967eb0
lmoquay <leo.moquay@travail.gouv.fr>
2022-12-07 10:53:09 +0000

    Suppresion Script1 + modif script2

--- scripts/script1.R
+++
@@ -1,5 +0,0 @@
-#### script 1 ####
-
-concat_mots <- function(mot1,mot2){
-  return(paste(mot1,mot2, sep = " "))
-}
--- scripts/script2.R
+++ scripts/script2.R
@@ -1,2 +1,5 @@
 #### script 2 ####
 
+concat_mots <- function(mot1,mot2){
+  return(paste(mot1,mot2, sep = " "))
+}
